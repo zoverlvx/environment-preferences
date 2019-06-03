c7d4f74d6dc3f0095a637a057f56666e51cf4667
zoverlvx <zoverhulser@gmail.com>
2019-06-03 01:46:03 +0000

    Automates getting files for environment.

---
+++ get-env.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/zoverlvx/environment-preferences/master/.bash_aliases
+wget https://raw.githubusercontent.com/zoverlvx/environment-preferences/master/.inputrc
+wget https://raw.githubusercontent.com/zoverlvx/environment-preferences/master/.vimrc
+
