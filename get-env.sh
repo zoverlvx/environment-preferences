7c773de961773f4e5c6047a4d6f4350485919974
Zachary Overhulser <zoverhulser@gmail.com>
2019-06-07 20:47:56 -0400

    Delete get-env.sh

--- get-env.sh
+++
@@ -1,4 +0,0 @@
-wget https://raw.githubusercontent.com/zoverlvx/environment-preferences/master/.bash_aliases
-wget https://raw.githubusercontent.com/zoverlvx/environment-preferences/master/.inputrc
-wget https://raw.githubusercontent.com/zoverlvx/environment-preferences/master/.vimrc
-
